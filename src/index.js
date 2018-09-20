18b6bd8d0aac0e5df610968b4d0e225eceb92ced
zhekaprb <zheka_cool@mail.ru>
2018-09-20 23:56:33 +0300

    100 tests passed

--- src/index.js
+++ src/index.js
@@ -4,4 +4,21 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+  let count=0;
+  let len = preferences.length;
+
+  for(let i=0; i < len; i++){
+    if (
+      preferences[preferences[preferences[i]-1]-1] === i+1 &&
+      preferences[i] !== preferences[preferences[i]-1] &&
+      preferences[preferences[i]-1] && preferences[preferences[preferences[i]-1]-1] &&
+      preferences[preferences[preferences[i]-1]-1] !== preferences[i]  
+    ) {
+       count++; 
+    }
+  }
+
+return (count/3);
 };
+
+
